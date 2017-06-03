17b2dcbb879cf1776b1cd4bb942e190ea5caae78
default-constructor <default-constructor@gmx.de>
2017-06-03 19:12:52 +0200

    camcapture-client initial commit

---
+++ camcapture-client/app/src/main/java/de/dc/camcapture_client/MainActivity.java
@@ -0,0 +1,227 @@
+package de.dc.camcapture_client;
+
+import android.content.res.AssetManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Process;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Properties;
+
+import de.dc.camcapture_client.utils.PermissionRequestHandler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private class ClientTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.d(TAG, "do in background");
+            try (Socket socket = new Socket(hostname, port);
+                 ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                 DataOutputStream dos = new DataOutputStream(socket.getOutputStream())
+            ) {
+                Log.i(TAG, "Connection " + ++count + " from server " + socket.getInetAddress() + ":" + socket.getPort());
+                dos.writeUTF("HTTP-Tunnel-Client version 0.0.1-SNAPSHOT");
+                while (true) {
+                    buffer = (byte[]) ois.readObject();
+                    if (0 == buffer.length) {
+                        dos.writeUTF("you gave me bullshit!");
+                        continue;
+                    }
+                    saveImage(buffer);
+                    dos.writeUTF("thank you!");
+                }
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private byte[] buffer;
+
+        private int count = 0;
+
+        private final String hostname;
+        private final int port;
+
+        private ClientTask(String hostname, int port) {
+            this.hostname = hostname;
+            this.port = port;
+        }
+    }
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    /*
+     * Main activity lifecycle
+     */
+
+    /**
+     * Called when the activity is first created. This is where you should do all of your normal
+     * static set up: create views, bind data to lists, etc. This method also provides you with a
+     * Bundle containing the activity's previously frozen state, if there was one. Always followed
+     * by onStart().
+     *
+     * @param savedInstanceState Bundle
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "on create");
+        permissionRequestHandler.checkPermissions();
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(v -> executeClientTask());
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * Called after your activity has been stopped, prior to it being started again. Always followed
+     * by onStart()
+     */
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "on restart");
+        super.onRestart();
+    }
+
+    /**
+     * Called when the activity is becoming visible to the user. Followed by onResume() if the
+     * activity comes to the foreground, or onStop() if it becomes hidden.
+     */
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "on start");
+        super.onStart();
+    }
+
+    /**
+     * Called when the activity will start interacting with the user. At this point your activity is
+     * at the top of the activity stack, with user input going to it. Always followed by onPause().
+     */
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "on resume");
+        super.onResume();
+    }
+
+    /**
+     * Called when the system is about to start resuming a previous activity. This is typically used
+     * to commit unsaved changes to persistent data, stop animations and other things that may be
+     * consuming CPU, etc. Implementations of this method must be very quick because the next
+     * activity will not be resumed until this method returns.
+     * Followed by either onResume() if the activity returns back to the front, or onStop() if it
+     * becomes invisible to the user.
+     */
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "on pause");
+        super.onPause();
+    }
+
+    /**
+     * Called when the activity is no longer visible to the user, because another activity has been
+     * resumed and is covering this one. This may happen either because a new activity is being
+     * started, an existing one is being brought in front of this one, or this one is being
+     * destroyed.
+     * Followed by either onRestart() if this activity is coming back to interact with the user, or
+     * onDestroy() if this activity is going away.
+     */
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "on stop");
+        super.onStop();
+    }
+
+    /**
+     * The final call you receive before your activity is destroyed. This can happen either because
+     * the activity is finishing (someone called finish() on it, or because the system is
+     * temporarily destroying this instance of the activity to save space. You can distinguish
+     * between these two scenarios with the isFinishing() method.
+     */
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "on destroy");
+        super.onDestroy();
+    }
+
+    // END Main activity lifecycle
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionRequestHandler.handlePermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    ImageView imageView;
+    Button button;
+
+    private final PermissionRequestHandler permissionRequestHandler;
+
+    public MainActivity() {
+        this.permissionRequestHandler = new PermissionRequestHandler(this);
+    }
+
+    private void executeClientTask() {
+        AssetManager assetMgr = getAssets();
+        try (InputStream is = assetMgr.open("app.properties")) {
+            Properties props = new Properties();
+            props.load(is);
+            String hostname = props.getProperty("server.hostname.local");
+            int port = Integer.parseInt(props.getProperty("server.port"));
+            Log.d(TAG, "Connecting to " + hostname + ":" + port);
+            ClientTask clientTask = new ClientTask(hostname, port);
+            clientTask.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    private void saveImage(byte[] bytes) {
+        if (!isExternalStorageWritable()) {
+            // TODO: error handling
+            return;
+        }
+        String dirname = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/CamCapture";
+        File dir = new File(dirname);
+        if (dir.mkdirs()) {
+            Log.d(TAG, "Created directory " + dirname);
+        }
+        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String filename = timestamp + ".jpg";
+        File imageFile = new File(dir, filename);
+        try (FileOutputStream fos = new FileOutputStream(imageFile)) {
+            fos.write(bytes);
+            fos.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ camcapture-client/app/src/main/java/de/dc/camcapture_client/utils/PermissionRequestHandler.java
@@ -0,0 +1,73 @@
+package de.dc.camcapture_client.utils;
+
+import android.Manifest;
+import android.app.Activity;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+
+import java.util.Arrays;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static android.support.v4.content.ContextCompat.checkSelfPermission;
+
+/**
+ * @author Thomas Reno
+ */
+public class PermissionRequestHandler {
+
+    public static final String TAG = PermissionRequestHandler.class.getSimpleName();
+
+    private static final int PERMISSION_REQUEST_SAVE_IMAGE = 0;
+
+    private boolean permissionGranted;
+
+    private final Activity context;
+
+    public PermissionRequestHandler(Activity context) {
+        this.context = context;
+    }
+
+    public void checkPermissions() {
+        Log.d(TAG, "check permissions");
+        int checkAccessNetworkState = checkSelfPermission(context, Manifest.permission.ACCESS_NETWORK_STATE);
+        int checkInternet = checkSelfPermission(context, Manifest.permission.INTERNET);
+        int checkWrite = checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (!(PERMISSION_GRANTED == (checkAccessNetworkState | checkInternet | checkWrite))) {
+            String[] permissions = new String[]{
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            };
+            context.requestPermissions(permissions, PERMISSION_REQUEST_SAVE_IMAGE);
+        }
+    }
+
+    public void handlePermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        Log.d(TAG, "handle permissions result");
+        switch (requestCode) {
+            case PERMISSION_REQUEST_SAVE_IMAGE:
+                Log.d(TAG, "permission request save image");
+                LoggerUtil.logPermissions(TAG, permissions);
+                permissionGranted = Arrays.stream(grantResults)
+                        .allMatch(grantResult -> PERMISSION_GRANTED == grantResult);
+                break;
+            default:
+                // nothing to do...
+        }
+        Log.d(TAG, "permission granted: " + permissionGranted);
+        if (!permissionGranted) {
+            AlertDialog.Builder adBuilder = new AlertDialog.Builder(context);
+            AlertDialog permissionDialog = adBuilder
+                    .setTitle("App requirements")
+                    .setMessage("This app needs permissions granted.")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", (dialog, which) -> {
+                        dialog.dismiss();
+                        context.finish();
+                        System.exit(0);
+                    })
+                    .create();
+            permissionDialog.show();
+        }
+    }
+}
