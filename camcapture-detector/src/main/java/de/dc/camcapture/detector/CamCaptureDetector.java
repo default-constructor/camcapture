32d0ae55021d58b0d5655956cb3779b11320352c
default-constructor <default-constructor@gmx.de>
2017-06-07 23:54:26 +0200

    pictures folder

--- camcapture-detector/src/main/java/de/dc/camcapture/detector/CamCaptureDetector.java
+++ camcapture-detector/src/main/java/de/dc/camcapture/detector/CamCaptureDetector.java
@@ -13,6 +13,12 @@ import de.dc.camcapture.detector.threads.DetectorThread;
 public class CamCaptureDetector {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CamCaptureDetector.class);
+	
+	private static final String VARIABLE_USERHOME = "user.home";
+	private static final String VARIABLE_PICTURES = "camcapture.pictures";
+
+	private static final String PATH_PICTURES = "/CamCapture/Pictures";
+	private static final String PATH_PICTURES_DEFAULT = System.getProperty(VARIABLE_USERHOME) + PATH_PICTURES;
 
 	private static final int CAMERA_INDEX = 0;
 
@@ -25,12 +31,12 @@ public class CamCaptureDetector {
 	}
 
 	private void start() {
-		LOG.info("Starting {} listening on device {}", CamCaptureDetector.class.getSimpleName(), CAMERA_INDEX);
-		String defaultPath = System.getProperty("user.home") + "/CamCapture/Pictures";
-		String path = System.getenv("CAMCAPTURE_PICTURES");
-		File dir = new File(null != path ? path : defaultPath);
-		dir.mkdirs();
-		Thread serverThread = new Thread(new DetectorThread(CAMERA_INDEX, CAMERA_WIDTH, CAMERA_HEIGHT, dir));
-		serverThread.start();
+		LOG.info("Starting {} -> Camera index {}", CamCaptureDetector.class.getSimpleName(), CAMERA_INDEX);
+		String path = System.getenv(VARIABLE_PICTURES);
+		File directory = new File(null != path ? path : PATH_PICTURES_DEFAULT);
+		directory.mkdirs();
+		DetectorThread detectorThread = new DetectorThread(CAMERA_INDEX, CAMERA_WIDTH, CAMERA_HEIGHT, directory);
+		Thread dThread = new Thread(detectorThread);
+		dThread.start();
 	}
 }
